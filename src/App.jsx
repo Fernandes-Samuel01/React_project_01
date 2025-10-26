db3c490a8a9edd3f9f31940f08fa35e86e2c0695
Samuel <fernandesamuel2002@gmail.com>
2025-10-26 14:00:10 +0530

    Counter program second commit with interview problem

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,19 @@ function App() {
     // }
 
     setCounter(counter + 1);
+
+    // // So looking at this we may think that the counter is increemented 5 times as it is coded but in reality all the 5 lines of code are doing the same one job that is increementing the counter by 1 so it is a bunch that does only one work and it is increemented by only one 
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // // So to fix this, when we want to increement it by 5 times we make use of arrow function because setCounter takes a callBack so we give it a callBack as previous value of counter to increement it by 5, so in this every time it increement the previous value of counter by 5  
+    // setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
   }
 
   function decreaseVal() {
